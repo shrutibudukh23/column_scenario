d3e57f357d0cadbdd64457f68c79d9c1649946f2
shrutibudukh23 <shrutibudukh23@gmail.com>
2024-10-08 19:12:06 +0530

    first

---
+++ column_scenarios/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(
+    const MainApp(),
+  );
+}
+class MainApp extends StatelessWidget{
+  const MainApp({super.key});
+
+@override
+  Widget build(BuildContext context){
+    print("Device width:${MediaQuery.of(context).size.width}");
+    print("Device height:${MediaQuery.of(context).size.height}");
+    return MaterialApp(
+      home:Scaffold(
+        appBar:AppBar(
+          title:const Text(
+            "Column Demo",
+            style:TextStyle(fontSize:30,fontWeight:FontWeight.w500),
+          ),
+          centerTitle:true,
+          backgroundColor: Colors.blue,
+          ),
+          body:Container(
+            width:MediaQuery.of(context).size.width,
+            color:Colors.blue,
+            child:Column(
+              mainAxisAlignment:MainAxisAlignment.start,
+              crossAxisAlignment:CrossAxisAlignment.end,
+              children: [
+                Container(
+                  height:200,
+                  width:200,
+                  color:Colors.amber,
+                ),
+              ],
+
+                ),
+          ),
+      ),
+    );
+  }
+}
+
+          
+
+
+    
